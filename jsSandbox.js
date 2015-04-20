acb7e152edfe878a19c1048f5ff8ccf91a943922
myNameisNotThis <robert.green.blue@gmail.com>
2015-04-20 09:38:23 -0500

    Discovered errors in my calculations
    I figured out that using the >= operator to test the left property on
    one of my colored divs was leading to inaccuracies when comparing to a
    value inside the leftPropertyTwo variable(interesting to note that this
    was not the case with the currentBox colored div and its leftProperty
    variable).I also realized that I forgot to place all the colored divs
    that are not being shown into single stack so that the amount of left I
    had to add to center them would be the same with each colored box.

--- jsSandbox.js
+++ jsSandbox.js
@@ -4,38 +4,57 @@ window.onload = function()	{
 		//boxArray is where I store all the ids for the colored boxes
 		var boxArray = [document.getElementById('box1'),document.getElementById('box2'),document.getElementById('box3'),document.getElementById('box4'),document.getElementById('box5'),document.getElementById('box6')];
 		
-		//currentBox is the currently-displayed colored box
+		//currentBox is the currently-displayed colored box,and nextBox is the box to be displayed after currentBox.
 		var currentBox = 0;
-		
-		//nextBox is the box to be displayed after currentBox.	
 		var nextBox = 1;
 		
-		//rightInterval1 is the variable where I will store the timer for moving the currently-displayed colored box to the right.
-		//rightInterval1 is declared globally so the clearInterval in the stopRight1 function can see it.		
-		var rightInterval1;
+		//Start: Variables for the hideBox function.
+		//rightInterval is the variable where I will store the timer for moving the currently-displayed colored box to the right.	
+		var rightInterval;
 		
-		//leftProperty1 is where I store the value I want to attach to currentBox's style.left property.
-		var leftProperty1 = 351;
+		//leftProperty is where I store the value I want to attach to currentBox's style.left property.
+		var leftProperty = 351;
 		
-		//endHere1 is the variable that stores the number which equates to the left value that I want to attach to the currentBox when it moves right.
-		var endHere1 = 801;
+		//endHere is the variable that stores the number which equates to the left value that I want to attach to the colored boxes moving right.
+		var endHere = 801;
+		//End: Variables for the hideBox function
 		
 		//This hides the current box
 		function hideBox()	{
-			boxArray[currentBox].style.left = leftProperty1 +  "px";
-			if(boxArray[currentBox].style.left >= endHere1 + "px")	{
-				stopRight1();
-				//boxArray[currentBox].style.left = -700 + "px";
+			boxArray[currentBox].style.left = leftProperty +  "px";
+			if(boxArray[currentBox].style.left === endHere + "px")	{
+				stopRight();
+				boxArray[currentBox].style.left = -700 + "px";
 			};
 		}
 		
+		//Start: Variables for the showBox function
+		//rightIntervalTwo is the showBox equivalent of the rightInterval variable above(see line 15).
+		var rightIntervalTwo;
+		
+		// leftPropertyTwo is the showBox equivalent of the leftProperty variable above(see line 18).
+		var leftPropertyTwo = -700;
+		
+		//endHereTwo is the showBox equivalent of the  endHere variable above(see line 21).
+		var endHereTwo =  251;
+		//End: Variables for the showBox.
+		
 		//This shows the next box
 		function showBox(box)	{
+			boxArray[nextBox].style.left = leftPropertyTwo + "px";
+			if(boxArray[nextBox].style.left === endHereTwo + "px")	{
+				stopRightTwo();
+			};
 		}
 		
 		//This is the clearInterval for the hideBox function
-		function stopRight1()	{
-			window.clearInterval(rightInterval1);
+		function stopRight()	{
+			window.clearInterval(rightInterval);
+		}
+		
+		//This is the clearInterval for the showBox function.
+		function stopRightTwo()	{
+			window.clearInterval(rightIntervalTwo);
 		}
 		
 		//This one combines the two functions for hiding and showing boxes
@@ -60,104 +79,22 @@ window.onload = function()	{
 				nextBox = 0;
 			};
 		}
+	
+	//This is the onclick function for going forwards	
+	document.getElementById('forwardButton').onclick = function()	{
+		rightInterval = window.setInterval(function()	{
+			hideBox();
+			leftProperty++;
+		},1);
 		
+		rightIntervalTwo = window.setInterval(function()	{
+			showBox();
+			leftPropertyTwo++;
+		},1);
+	}	
 
 
 //These are my observations on the slidewhow code on the next two lines
 //0,1,click,hide,show,check 1,2,click,hide,show,check 2,3,click,hide,show,check 3,4,click,hide,show,check <-- yellow and purple  4,5
 //currentBox now equals 0,but with purple currently showing,it ignores the purple and hides an already-hidden blue and skips to red next time I click it
-			
-//End: Targeting section of code
-	
-	//This is the onclick function for going forwards	
-	document.getElementById('forwardButton').onclick = function()	{
-		rightInterval1 = window.setInterval(function()	{
-			hideBox();
-			leftProperty1++;
-		},1);
-	}
-		
-//This is the end of the slideshow code.
-
-
-
-
-
-
-
-
-
-
-
-
-/*var one = alert(8 + 19 + 26 + 34 + 43 + 40 + 37 + 56 + 50);
-			var two = alert(9 + 11 + 16 + 15 + 22 + 20 + 16 + 20 + 19);
-			alert(8 + 19 + 26 + 34 + 43 + 40 + 37 + 56 + 50 + 9 + 11 + 16 + 15 + 22 + 20 + 16 + 20 + 19);*/
-
-
-			/*My code
-			switch (currentDisplay)	{
-				case document.getElementById("box1"):
-					hideBox(document.getElementById("box1"));
-					showBox(document.getElementById("box2"));
-					currentDisplay = document.getElementById("box2");
-					break;
-				case document.getElementById("box2"):
-					hideBox(document.getElementById("box2"));
-					showBox(document.getElementById("box3"));
-					currentDisplay = document.getElementById("box3");
-					break;
-				case document.getElementById("box3"):
-					hideBox(document.getElementById("box3"));
-					showBox(document.getElementById("box4"));
-					currentDisplay = document.getElementById("box4");
-					break;
-				case document.getElementById("box4"):
-					hideBox(document.getElementById("box4"));
-					showBox(document.getElementById("box5"));
-					currentDisplay = document.getElementById("box5");
-					break;
-				case document.getElementById("box5"):
-					hideBox(document.getElementById("box5"));
-					showBox(document.getElementById("box1"));
-					currentDisplay = document.getElementById("box1");
-					break;
-				default:
-					alert("HELLO");
-			}*/
-
-
-		
-			/*Dad's Stuff
-			
-			if(currentBox.style.display = "block" && counter < currentBoxberOfBoxes)	{ 
-			
-			
-				currentBox.style.display = "none";
-				currentBox = currentBox + 1;
-				alert(currentBox);
-				//currentBox.style.display = "block";
-
-				counter++;
-			}
-			
-			//for(i = 0; i < boxArray.length; i++)	{
-				//alert('hello');
-				//if(boxArray[i].style.display = "block")	{
-				//	hideBox(boxArray[i]);
-				//	console.log(boxArray[i]);
-				//	showBox(boxArray[i + 1]);
-				//	break;
-				//} else	{
-					//alert("Try Something Else.");
-				//};
-			//};	*/		
-			
-			
-		
-	
-		
 }
-
-	
-	//This is outside the onclick function
