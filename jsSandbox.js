ed88533c09b350421130f1600cffe683cb3f3e73
myNameisNotThis <robert.green.blue@gmail.com>
2015-05-14 09:41:16 -0500

    Began work on moving nextBox right.
    I renamed some of the variables used in the original moveRight function
    so that I could mirror that code to move the nextBox div.

--- jsSandbox.js
+++ jsSandbox.js
@@ -1,43 +1,50 @@
 window.onload = function()	{
-	var box1 = document.getElementById('box1');
-	var box2 = document.getElementById('box2');
-	var box3 = document.getElementById('box3');
-	var box4 = document.getElementById('box4');
-	var box5 = document.getElementById('box5');
-	var box6 = document.getElementById('box6');
 	
 	var boxArray = [document.getElementById('box1'),document.getElementById('box2'),document.getElementById('box3'),document.getElementById('box4'),document.getElementById('box5'),document.getElementById('box6')];
-	var centerToRight = 449;
+	var centerToRight = 446;
 	var leftToCenter = 456;
 	
 	var currentBox = 0;
 	var nextBox = 1;
+
 	
-	var getLeft = window.getComputedStyle(boxArray[currentBox],null).getPropertyValue("left");
-	console.log(getLeft);
-	var leftOne = parseInt(getLeft);
+
 	
-	function someThing()	{
-		function stop()	{
-			clearInterval(firstInterval);
-			counter = 0;
-		}
+	document.getElementById('forwardButton').onclick = function()	{
+		moveRight();
+	}
+	
+	//stop1 and stop is the function to clear the setInterval that I'm using.firstInterval repeatedly adds to the leftOne variable and appends its value to the left property of the box I want to move(currentBox)
+	//it does that until it reaches a limit(the absolute amount of pixels it takes to traverse the space from center to right,calculated by counter variable)and calls the stop function.
+	function moveRight()	{
+		//Start: part of function for moving currentBox right.
+		var getLeft1 = window.getComputedStyle(boxArray[currentBox],null).getPropertyValue("left");
+		var leftOne = parseInt(getLeft1);
+		var counter1 = 0;
 		
-		var counter = 0;
+		function stop1()	{
+			clearInterval(firstInterval);
+			counter1 = 0;
+			leftOne = 0;
+			findNext();
+		}
 		
 		var firstInterval = window.setInterval(function()	{
 			boxArray[currentBox].style.left = leftOne + "px";
 			leftOne++;
-			counter++;
-			if (counter == 446)	{
-				stop();
-				console.log("Too effective");
+			counter1++;
+			if (counter1 == 447)	{
+				stop1();
 			};
 		},1);
+		//End: part of function for moving currentBox right
 		
+		//Start: part of function for moving nextBox right
+		var getLeft2 = window.getComputedStyle(boxArray[nextBox],null).getPropertyValue("left");
+		
+		//End: part of function for moving nextBox right
 		
 	}
-	someThing();
 	
 	//This adds to the 'current' box and the 'next' box counters.
 	function findNext()	{
